1324564d2e7486acc77e946ec88709e1e4ddcb12
javiluli <javiluli.dev@gmail.com>
2026-03-26 23:43:11 +0100

    add Vercel analysis

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
+import App from '@/App'
+import type { Language } from '@/shared/@types/i18n'
+import i18n from '@/shared/i18n/i18n'
+import { useDataStore } from '@/store/data.store'
 import { HeroUIProvider } from '@heroui/react'
+import { Analytics } from '@vercel/analytics/react'
 import '@xyflow/react/dist/style.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from '@/App'
-import i18n from '@/shared/i18n/i18n'
-import type { Language } from '@/shared/@types/i18n'
-import { useDataStore } from '@/store/data.store'
 import './index.css'
 
 const syncDataLanguage = () => {
@@ -21,6 +22,7 @@ syncDataLanguage()
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HeroUIProvider>
+      <Analytics />
       <main className="dark text-foreground bg-background">
         <App />
       </main>
